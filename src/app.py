077009e5c35df5c9d640ffa9b38a9cac26672d95
asheikh <asim.sardar.sheikh@gmail.com>
2022-09-26 12:55:09 +1000

    Moving to blog model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from repo import MemRepo, Task
+from repo import MemRepo, Blog
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -13,16 +13,11 @@ def index():
 def api():
     if request.method == 'POST':
         data =  request.json or {}
-        if data.get('name'):
-            name = data['name'] or ''
-            task = Task(name=name)
-            repo.add_task(task)
-            tasks = repo.get_tasks()
-            return f'{"".join(str(task.name) for task in tasks)}'
-        if data.get('blog'):
-            blog = data['blog']
-            title = data['title']
-            return f'<p>{title}</p><p>{blog}</p>'
-
+        name = data['name'] or ''
+        task = Task(name=name)
+        repo.add_task(task)
+        tasks = repo.get_tasks()
+            
+        return f'{"".join(str(task.name) for task in tasks)}'
     return 'No data'
 
--- test/blog_test.py
+++ test/blog_test.py
@@ -6,12 +6,12 @@ def test_initial():
 
 def test_can_add_todo():
     repo = MemRepo()
-    task = Task(name="Go shopping")
-    repo.add_task(task)
-    assert repo.get_tasks() == [task]
+    blog = Blog(title='First blog', blog='Entering some information')
+    repo.add_blog(blog)
+    assert repo.get_blogs() == [blog]
 
-    repo.add_task(task)
-    assert repo.get_tasks() == [task,task]
+    repo.add_blog(blog)
+    assert repo.get_blogs() == [blog, blog]
 
 def test_flask_app_running():
     assert 'Blog' in requests.get('http://127.0.0.1:5000/').text
